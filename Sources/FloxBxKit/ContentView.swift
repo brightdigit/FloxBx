7088d1ffc7ce1e50b5750942cc214951335dc8e3
Leo Dion <leogdion@brightdigit.com>
2021-05-11 16:01:03 -0400

    getting ready for app

---
+++ Sources/FloxBxKit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Leo Dion on 5/10/21.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+  public init () {}
+  
+    public var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+public struct ContentView_Previews: PreviewProvider {
+  public static var previews: some View {
+        ContentView()
+    }
+}
