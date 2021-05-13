b21a47eeeaa777d9a802b3b477b245c5f68e8729
Leo Dion <leogdion@brightdigit.com>
2021-05-13 07:44:05 -0400

    adding authentication

---
+++ Sources/FloxBxServerKit/Models/User.swift
@@ -0,0 +1,46 @@
+import Fluent
+import Vapor
+
+final class User: Model {
+  
+  static let schema = "Users"
+  struct FieldKeys {
+      static var email: FieldKey { "email" }
+      static var password: FieldKey { "password" }
+  }
+
+  @ID() var id: UUID?
+  @Field(key: FieldKeys.email) var email: String
+  @Field(key: FieldKeys.password) var password: String
+
+    init() { }
+
+  init(id: User.IDValue? = nil,
+       email: String,
+       password: String)
+  {
+      self.id = id
+      self.email = email
+      self.password = password
+  }
+}
+
+extension User : ModelAuthenticatable {
+  static var usernameKey: KeyPath<User, Field<String>> = \.$email
+  
+  static let passwordHashKey: KeyPath<User, Field<String>>  = \.$password
+  
+  func verify(password: String) throws -> Bool {
+    try Bcrypt.verify(password, created: self.password)
+  }
+  
+  
+}
+extension User {
+  func generateToken() throws -> UserToken {
+    try .init(
+      value: [UInt8].random(count: 16).base64,
+      userID: requireID()
+    )
+  }
+}
