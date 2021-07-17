60d293b90164b1b5e0018416e1d73b4cc968e61d
Leo Dion <leogdion@brightdigit.com>
2021-07-17 18:41:38 -0400

    working on adding sentry

--- Sources/FloxBxKit/Application.swift
+++ Sources/FloxBxKit/Application.swift
@@ -7,6 +7,25 @@
 
 import SwiftUI
 
+import SentryCocoa
+import SentryVanilla
+
+public class Sentry {
+  public static func start () {
+    try? SentryVanilla.Sentry.start { options in
+      options.dsn = "https://d2a8d5241ccf44bba597074b56eb692d@o919385.ingest.sentry.io/5868822"
+      
+      //options.debug = true // Enabled debug when first installing is always helpful
+    }
+    SentryVanilla.Sentry.capture(event: .init(message: "Hello World", tags: nil), configureScope: nil)
+//    
+//    SentryCocoa.SentrySDK.start { options in
+//      options.dsn
+//      options.de
+//    }
+  }
+}
+
 public protocol Application: App {
   
 }
