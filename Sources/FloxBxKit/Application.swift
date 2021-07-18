9a19bd889b1f9e610d301fbafb75bc59782d08cf
Leo Dion <leogdion@brightdigit.com>
2021-07-18 13:45:10 -0400

    support for server and client

--- Sources/FloxBxKit/Application.swift
+++ Sources/FloxBxKit/Application.swift
@@ -6,17 +6,55 @@
 //
 
 
-//import SentryCocoa
+#if os(Linux) || os(macOS)
 import SentryVanilla
+#endif
 
+#if !os(Linux)
+import SentryCocoa
+#endif
+
+public enum ApplicationType {
+  #if os(Linux) || os(macOS)
+  case server
+  #endif
+#if !os(Linux)
+  case client
+  #endif
+}
 public class Sentry {
-  public static func start () {
-    try? SentryVanilla.Sentry.start { options in
-      options.dsn = "https://d2a8d5241ccf44bba597074b56eb692d@o919385.ingest.sentry.io/5868822"
-      
-      //options.debug = true // Enabled debug when first installing is always helpful
+  public static func start (_ type: ApplicationType) {
+    switch type {
+  #if os(Linux) || os(macOS)
+    case .server:
+      try? SentryVanilla.Sentry.start { options in
+        options.dsn = "https://d2a8d5241ccf44bba597074b56eb692d@o919385.ingest.sentry.io/5868822"
+        
+        //options.debug = true // Enabled debug when first installing is always helpful
+      }
+      SentryVanilla.Sentry.capture(event: .init(message: "Hello World", tags: nil), configureScope: nil)
+      #endif
+#if !os(Linux)
+    case .client:
+      SentrySDK.start { options in
+        options.dsn = "https://d2a8d5241ccf44bba597074b56eb692d@o919385.ingest.sentry.io/5868822"
+      }
+      SentrySDK.capture(message: "Hello World")
+      #endif
     }
-    SentryVanilla.Sentry.capture(event: .init(message: "Hello World", tags: nil), configureScope: nil)
+//    #if canImport(SentryCocoa)
+//    SentrySDK.start { options in
+//      options.dsn = "https://d2a8d5241ccf44bba597074b56eb692d@o919385.ingest.sentry.io/5868822"
+//    }
+//    SentrySDK.capture(message: "Hello World")
+//    #else
+//    try? SentryVanilla.Sentry.start { options in
+//      options.dsn = "https://d2a8d5241ccf44bba597074b56eb692d@o919385.ingest.sentry.io/5868822"
+//
+//      //options.debug = true // Enabled debug when first installing is always helpful
+//    }
+//    SentryVanilla.Sentry.capture(event: .init(message: "Hello World", tags: nil), configureScope: nil)
+//    #endif
 //    
 //    SentryCocoa.SentrySDK.start { options in
 //      options.dsn
@@ -27,6 +65,7 @@ public class Sentry {
 
 #if canImport(SwiftUI)
 import SwiftUI
+
 public protocol Application: App {
   
 }
@@ -34,7 +73,9 @@ public protocol Application: App {
 public extension Application {
   var body: some Scene {
       WindowGroup {
-        ContentView().environmentObject(ApplicationObject())
+        ContentView().environmentObject(ApplicationObject()).onAppear {
+          Sentry.start(.client)
+        }
       }
   }
 }
--- Sources/FloxBxServerKit/Server.swift
+++ Sources/FloxBxServerKit/Server.swift
@@ -19,7 +19,7 @@ public struct Server {
   public static func configure(_ app: Application) throws {
       // uncomment to serve files from /Public folder
       // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    Sentry.start()
+    Sentry.start(.server)
       app.databases.use(.postgres(
           hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         username: Environment.get("DATABASE_USERNAME") ?? "floxbx", password: ""
