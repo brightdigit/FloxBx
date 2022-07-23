9b1c616cdf881923689d4af885a576e882e6379a
Leo Dion <leogdion@brightdigit.com>
2022-07-22 21:01:45 -0400

    fixing server-side call

--- Sources/FloxBxServerKit/Controllers/TodoController.swift
+++ Sources/FloxBxServerKit/Controllers/TodoController.swift
@@ -48,35 +48,62 @@ struct TodoController: RouteCollection {
 
   func create(from request: Request) throws -> EventLoopFuture<CreateTodoResponseContent> {
     let user = try request.auth.require(User.self)
-    //let userID = try user.requireID()
     let content = try request.content.decode(CreateTodoRequestContent.self)
     let todo = Todo(title: content.title)
     
-    //let userF : EventLoopFuture<User> = user
-    
-    return user.$items.create(todo, on: request.db).flatMapThrowing {
-      try CreateTodoResponseContent(id: todo.requireID(), title: todo.title)
+    let userF : EventLoopFuture<User>
+    if let sessionID: UUID = request.parameters.get("sessionID", as: UUID.self) {
+      let session = GroupSession.find(sessionID, on: request.db).unwrap(or: Abort(.notFound))
+      userF = session.map{$0.user}
+    } else {
+      userF = request.eventLoop.makeSucceededFuture(user)
     }
+    
+    return userF.flatMap { user in
+      return user.$items.create(todo, on: request.db).flatMapThrowing {
+        try CreateTodoResponseContent(id: todo.requireID(), title: todo.title)
+      }
+    }
+    
   }
 
   func update(from request: Request) throws -> EventLoopFuture<HTTPResponseStatus> {
     let user = try request.auth.require(User.self)
     let todoID: UUID = try request.parameters.require("todoID", as: UUID.self)
     let content = try request.content.decode(CreateTodoRequestContent.self)
-    return user.$items.query(on: request.db)
-      .filter(\.$id == todoID)
-      .first().unwrap(orError: Abort(.notFound))
-      .flatMap { todo -> EventLoopFuture<Void> in
-        todo.title = content.title
-        return todo.update(on: request.db)
-      }.transform(to: .noContent)
+    let userF : EventLoopFuture<User>
+    if let sessionID: UUID = request.parameters.get("sessionID", as: UUID.self) {
+      let session = GroupSession.find(sessionID, on: request.db).unwrap(or: Abort(.notFound))
+      userF = session.map{$0.user}
+    } else {
+      userF = request.eventLoop.makeSucceededFuture(user)
+    }
+    return userF.flatMap { user in
+      user.$items.query(on: request.db)
+        .filter(\.$id == todoID)
+        .first().unwrap(orError: Abort(.notFound))
+        .flatMap { todo -> EventLoopFuture<Void> in
+          todo.title = content.title
+          return todo.update(on: request.db)
+        }.transform(to: .noContent)
+    }
   }
 
   func delete(from request: Request) throws -> EventLoopFuture<HTTPStatus> {
+    
     let user = try request.auth.require(User.self)
     let todoID: UUID = try request.parameters.require("todoID", as: UUID.self)
-    return user.$items.query(on: request.db).filter(\.$id == todoID).all()
-      .flatMap { $0.delete(on: request.db) }
-      .transform(to: .noContent)
+    let userF : EventLoopFuture<User>
+    if let sessionID: UUID = request.parameters.get("sessionID", as: UUID.self) {
+      let session = GroupSession.find(sessionID, on: request.db).unwrap(or: Abort(.notFound))
+      userF = session.map{$0.user}
+    } else {
+      userF = request.eventLoop.makeSucceededFuture(user)
+    }
+    return userF.flatMap { user in
+      user.$items.query(on: request.db).filter(\.$id == todoID).all()
+        .flatMap { $0.delete(on: request.db) }
+        .transform(to: .noContent)
+    }
   }
 }
