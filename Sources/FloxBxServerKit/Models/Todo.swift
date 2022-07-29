ce4e0971f10b5c9743256e83bf01553b86f02f7e
Leo Dion <leogdion@brightdigit.com>
2022-07-29 15:20:13 -0400

    rename GroupSession and Todo

--- Sources/FloxBxServerKit/Models/Todo.swift
+++ Sources/FloxBxServerKit/Models/Todo.swift
@@ -1,49 +1,29 @@
 import Fluent
 import Vapor
 
-final class GroupSession: Model, Content {
-  static let schema = "GroupSessions"
+final class Todo: Model, Content {
+  static let schema = "Todos"
   enum FieldKeys {
+    static let title: FieldKey = "title"
     static let userID: FieldKey = "userID"
   }
 
   @ID(key: .id)
   var id: UUID?
 
+  @Field(key: "title")
+  var title: String
+
   @Parent(key: FieldKeys.userID)
   var user: User
 
   init() {}
 
-  init(id: UUID? = nil, userID: UUID) {
+  init(id: UUID? = nil, title: String, userID: UUID? = nil) {
     self.id = id
-    $user.id = userID
-  }
-}
-
-extension GroupSession {
-  static func user(forGroupSessionWithID groupSessionID: UUID?, otherwise user: User, on db: Database, eventLoop: EventLoop) -> EventLoopFuture<User> {
-    let userF: EventLoopFuture<User>
-    if let sessionID: UUID = groupSessionID {
-      let session = GroupSession.find(sessionID, on: db).unwrap(orError: Abort(.notFound))
-      userF = session.flatMap {
-        $0.$user.get(on: db)
-      }
-    } else {
-      userF = eventLoop.makeSucceededFuture(user)
+    self.title = title
+    if let userID = userID {
+      $user.id = userID
     }
-    return userF
-  }
-
-  static func user(fromRequest request: Request, otherwise user: User) -> EventLoopFuture<User> {
-    let sessionID = request.parameters.get("sessionID", as: UUID.self)
-    let database = request.db
-    let eventLoop = request.eventLoop
-    return self.user(
-      forGroupSessionWithID: sessionID,
-      otherwise: user,
-      on: database,
-      eventLoop: eventLoop
-    )
   }
 }
