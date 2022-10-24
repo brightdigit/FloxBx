4d2f542d34279f1f7c92f76f369f0b450e6b74d4
Leo Dion <leogdion@brightdigit.com>
2022-10-24 14:44:07 -0400

    adding GroupActivitySharingView

---
+++ Sources/FloxBxGroupActivities/GroupActivitySharingView.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Leo Dion on 10/24/22.
+//
+
+import SwiftUI
+import UIKit
+import GroupActivities
+
+@available(iOS 15.4, *)
+struct GroupActivitySharingView<ActivityType : GroupActivity> : UIViewControllerRepresentable {
+  internal init(activity: ActivityType) throws {
+    self.controller = try GroupActivitySharingController(activity)
+  }
+  
+  let controller : GroupActivitySharingController
+  
+  
+  func makeUIViewController(context: Context) -> GroupActivitySharingController {
+    return controller
+    
+    
+  }
+  
+  func updateUIViewController(_ uiViewController: GroupActivitySharingController, context: Context) {
+    
+  }
+  
+  typealias UIViewControllerType = GroupActivitySharingController
+  
+  
+}
