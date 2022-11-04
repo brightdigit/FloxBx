c59c1f8ad18ac7d6f4bede3ef8e7eb80dcefb76e
Leo Dion <leogdion@brightdigit.com>
2022-11-04 19:58:14 -0400

    adding linting files

---
+++ scripts/lint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+export MINT_PATH="$PWD/.mint"
+MINT_ARGS="-n -m ../../Mintfile --silent"
+MINT_RUN="/opt/homebrew/bin/mint run $MINT_ARGS"
+
+/opt/homebrew/bin/mint bootstrap
+
+if [ -z "$SRCROOT" ]; then
+	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+	PACKAGE_DIR="${SCRIPT_DIR}/../Packages/FloxBxKit"
+else
+	PACKAGE_DIR="${SRCROOT}/Packages/FloxBxKit" 	
+fi
+
+
+if [ "$LINT_MODE" == "NONE" ]; then
+	exit
+elif [ "$LINT_MODE" == "STRICT" ]; then
+	SWIFTFORMAT_OPTIONS=""
+	SWIFTLINT_OPTIONS="--strict"
+	STRINGSLINT_OPTIONS="--config .strict.stringslint.yml"
+else 
+	SWIFTFORMAT_OPTIONS=""
+	SWIFTLINT_OPTIONS=""
+	STRINGSLINT_OPTIONS="--config .stringslint.yml"
+fi
+
+pushd $PACKAGE_DIR
+
+if [ -z "$CI" ]; then
+	$MINT_RUN swiftformat .
+	$MINT_RUN swiftlint autocorrect
+else 
+	set -e
+fi
+
+$MINT_RUN periphery scan 
+$MINT_RUN stringslint lint $STRINGSLINT_OPTIONS
+$MINT_RUN swiftformat --lint $SWIFTFORMAT_OPTIONS .
+$MINT_RUN swiftlint lint $SWIFTLINT_OPTIONS
+
+popd
