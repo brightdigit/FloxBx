18068114b7257a7d8599e5a76f737456abbda03e
Leo Dion <leogdion@brightdigit.com>
2022-11-23 20:42:59 -0500

    fixing sublimation version

--- Packages/FloxBxKit/Package.swift
+++ Packages/FloxBxKit/Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
     .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
     .package(url: "https://github.com/brightdigit/Canary.git", from: "0.2.0-beta.1"),
-    .package(url: "https://github.com/brightdigit/Sublimation.git", from: "0.1.1")
+    .package(url: "https://github.com/brightdigit/Sublimation.git", from: "0.1.2")
   ],
   targets: [
     .executableTarget(
