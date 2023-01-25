c320f325fe840bb7a975a8a53b68534b6c214aea
Leo Dion <leogdion@brightdigit.com>
2023-01-25 17:56:59 -0500

    spliting Loggers

--- Packages/FloxBxKit/Sources/FloxBxLogging/Loggers.swift
+++ Packages/FloxBxKit/Sources/FloxBxLogging/Loggers.swift
@@ -1,4 +1,3 @@
-
 import FelinePine
 
 #if canImport(os)
@@ -7,18 +6,6 @@ import FelinePine
   import Logging
 #endif
 
-public enum LoggerCategory : String, CaseIterable {
-  case reactive
-  // swiftlint:disable:next identifier_name
-  case ui
-  case userDefaults
-  case watchConnectivity
-  case authentication
-  case networking
-  case server
-  case keychain
-  case shareplay
-}
 
 public struct Loggers : FelinePine.Loggers {
   public static var loggers: [LoggerCategory : Logger] {
