bf72e3e3f1f8a54fb93bd75c3bb2d104fdaf5873
Leo Dion <leogdion@brightdigit.com>
2023-06-07 12:42:47 -0400

    git subrepo pull Packages/FloxBxKit/Packages/FelinePine
    subrepo:
      subdir:   "Packages/FloxBxKit/Packages/FelinePine"
      merged:   "957f9b3"
    upstream:
      origin:   "git@github.com:brightdigit/FelinePine.git"
      branch:   "floxbx"
      commit:   "957f9b3"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/Homebrew/brew"
      commit:   "8917acac9"

--- Packages/FloxBxKit/Packages/FelinePine/Package.swift
+++ Packages/FloxBxKit/Packages/FelinePine/Package.swift
@@ -1,7 +1,17 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
+let swiftSettings: [SwiftSetting] = [
+   .enableUpcomingFeature("BareSlashRegexLiterals"),
+   .enableUpcomingFeature("ConciseMagicFile"),
+   .enableUpcomingFeature("ExistentialAny"),
+   .enableUpcomingFeature("ForwardTrailingClosures"),
+   .enableUpcomingFeature("ImplicitOpenExistentials"),
+   .enableUpcomingFeature("StrictConcurrency"),
+   .unsafeFlags(["-warn-concurrency", "-enable-actor-data-race-checks"]),
+]
 
 let package = Package(
     name: "FelinePine",
@@ -15,7 +25,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FelinePine"),
+            name: "FelinePine",
+             swiftSettings: swiftSettings),
         .testTarget(
             name: "FelinePineTests",
             dependencies: ["FelinePine"]),
